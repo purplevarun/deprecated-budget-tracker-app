cbb8944a7fff63c58c5de89f292f1fa046f8329d
Varun Kedia <purplevarun@gmail.com>
2022-07-22 22:45:56 +0530

    Added Header Component

---
+++ components/header/Header.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+const Header = () => {
+	return (
+		<View
+			style={{ height: 100, backgroundColor: "red", display: "flex" }}
+		></View>
+	);
+};
+
+export default Header;
