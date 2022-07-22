a306ef88ff7e81052edc82803cddd7f5bec1215c
Varun Kedia <purplevarun@gmail.com>
2022-07-22 22:06:55 +0530

    Added HomeScreen

---
+++ screens/home/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { Button, Text, View } from "react-native";
+
+const HomeScreen = () => {
+	return (
+		<View
+			style={{
+				display: "flex",
+				height: "100%",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Text>Hello World</Text>
+		</View>
+	);
+};
+export default HomeScreen;
