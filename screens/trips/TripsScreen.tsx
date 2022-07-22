5550af360c3e0ea78b5fdc55d117f8dfe7a0047b
Varun Kedia <purplevarun@gmail.com>
2022-07-22 22:07:01 +0530

    Added TripsScreen

---
+++ screens/trips/TripsScreen.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const TripsScreen = () => {
+	return (
+		<View
+			style={{
+				display: "flex",
+				height: "100%",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Text>trips</Text>
+		</View>
+	);
+};
+export default TripsScreen;
