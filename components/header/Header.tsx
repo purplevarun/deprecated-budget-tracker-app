dae3f150040ee39e21215873ecb2db8f77b18e79
Varun Kedia <purplevarun@gmail.com>
2022-07-23 16:16:29 +0530

    Added Grid Icon in Header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,20 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import IonIcons from "react-native-vector-icons/Ionicons";
 
 const Header = () => {
 	return (
 		<View
-			style={{ height: 100, backgroundColor: "red", display: "flex" }}
-		></View>
+			style={{
+				height: 80,
+				backgroundColor: "red",
+				display: "flex",
+				flexDirection: "row",
+				alignItems: "center",
+				paddingLeft: 20,
+			}}
+		>
+			<IonIcons name="grid" size={40} />
+		</View>
 	);
 };
 
