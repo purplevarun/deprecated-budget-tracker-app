14456c9b3741f73d231f32c8302c04c8c046b4ba
Varun Kedia <purplevarun@gmail.com>
2022-07-23 16:16:05 +0530

    Added Buffer in Wrapper

--- navigator/Wrapper.tsx
+++ navigator/Wrapper.tsx
@@ -1,10 +1,12 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import Header from "../components/header/Header";
 import Navigator from "./Navigator";
+import Buffer from "../components/buffer/Buffer";
 
 const Wrapper = () => {
 	return (
 		<View style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+			<Buffer />
 			<Header />
 			<View style={{ height: "100%", display: "flex", flexDirection: "row" }}>
 				<Navigator />
