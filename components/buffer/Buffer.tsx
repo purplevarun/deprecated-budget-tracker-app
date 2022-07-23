2560187bbd397b4766fe78d338fc0e1fa5323ae2
Varun Kedia <purplevarun@gmail.com>
2022-07-23 16:01:48 +0530

    Added Buffer for developement

---
+++ components/buffer/Buffer.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const Buffer = () => {
+	return (
+		<View
+			style={{ height: 30, justifyContent: "center", alignItems: "center" }}
+		>
+			<Text>Buffer for developement</Text>
+		</View>
+	);
+};
+
+export default Buffer;
