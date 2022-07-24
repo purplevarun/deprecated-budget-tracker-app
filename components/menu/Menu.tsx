b2ef4737aae52cf450be1cb83255be7a1e897d53
Varun Kedia <purplevarun@gmail.com>
2022-07-24 14:04:34 +0530

    Extracted Contacts into Component

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Animated, Linking, Text, TouchableOpacity, View } from "react-native";
 import useData from "../../context/useData";
-import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import Contacts from "./components/Contacts";
 const Menu = () => {
 	const { menuOpen, translateX, menuWidth, colors } = useData();
 
@@ -36,67 +36,7 @@ const Menu = () => {
 				<Text>sadhsaui</Text>
 				<Text>safdmnsa</Text>
 			</View>
-			<View
-				style={{
-					paddingBottom: 10,
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<Text style={{ fontSize: 15 }}>Made with ❤️ by Varun Kedia</Text>
-				<View
-					style={{
-						display: "flex",
-						flexDirection: "row",
-					}}
-				>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("https://appopener.com/lk/y7jcn9z4f");
-						}}
-						style={{ padding: 5 }}
-					>
-						<AntDesign name="linkedin-square" size={30} color={colors.fg} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("https://appopener.com/ig/f202zopfl");
-						}}
-						style={{ padding: 5 }}
-					>
-						<AntDesign name="instagram" size={30} color={colors.fg} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("mailto:purplevarun@gmail.com");
-						}}
-						style={{ padding: 5 }}
-					>
-						<AntDesign name="mail" size={30} color={colors.fg} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("https://github.com/purplevarun");
-						}}
-						style={{ padding: 5 }}
-					>
-						<AntDesign name="github" size={30} color={colors.fg} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => {
-							Linking.openURL("https://purplevarun.github.io/");
-						}}
-						style={{ padding: 5 }}
-					>
-						<MaterialCommunityIcons
-							name="face-man-profile"
-							size={30}
-							color={colors.fg}
-						/>
-					</TouchableOpacity>
-				</View>
-			</View>
+			<Contacts />
 		</Animated.View>
 	);
 };
---
+++ components/menu/components/Contacts.tsx
@@ -0,0 +1,73 @@
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Linking, Text, TouchableOpacity, View } from "react-native";
+import useData from "../../../context/useData";
+
+const Contacts = () => {
+	const { colors } = useData();
+
+	return (
+		<View
+			style={{
+				paddingBottom: 10,
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+			}}
+		>
+			<Text style={{ fontSize: 15 }}>Made with ❤️ by Varun Kedia</Text>
+			<View
+				style={{
+					display: "flex",
+					flexDirection: "row",
+				}}
+			>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL("https://appopener.com/lk/y7jcn9z4f");
+					}}
+					style={{ padding: 5 }}
+				>
+					<AntDesign name="linkedin-square" size={30} color={colors.fg} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL("https://appopener.com/ig/f202zopfl");
+					}}
+					style={{ padding: 5 }}
+				>
+					<AntDesign name="instagram" size={30} color={colors.fg} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL("mailto:purplevarun@gmail.com");
+					}}
+					style={{ padding: 5 }}
+				>
+					<AntDesign name="mail" size={30} color={colors.fg} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL("https://github.com/purplevarun");
+					}}
+					style={{ padding: 5 }}
+				>
+					<AntDesign name="github" size={30} color={colors.fg} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL("https://purplevarun.github.io/");
+					}}
+					style={{ padding: 5 }}
+				>
+					<MaterialCommunityIcons
+						name="face-man-profile"
+						size={30}
+						color={colors.fg}
+					/>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+export default Contacts;
