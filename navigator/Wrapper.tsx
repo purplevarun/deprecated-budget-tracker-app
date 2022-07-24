cd870829f6ed9e385bd2911ed009161e0dfec71a
Varun Kedia <purplevarun@gmail.com>
2022-07-24 15:11:16 +0530

    Extracted LoginConditional to Component

--- navigator/Wrapper.tsx
+++ navigator/Wrapper.tsx
@@ -1,18 +1,14 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import Header from "../components/header/Header";
-import Navigator from "./Navigator";
 import Buffer from "../components/buffer/Buffer";
-import Menu from "../components/menu/Menu";
+import LoginConditional from "./LoginConditional";
 
 const Wrapper = () => {
 	return (
 		<View style={{ height: "100%", display: "flex", flexDirection: "column" }}>
 			<Buffer />
 			<Header />
-			<View style={{ height: "100%", display: "flex", flexDirection: "row" }}>
-				<Menu />
-				<Navigator />
-			</View>
+			<LoginConditional />
 		</View>
 	);
 };
