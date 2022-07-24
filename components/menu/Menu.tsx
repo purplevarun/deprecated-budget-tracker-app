4f69fd4fbfd637c7d5b112739214bb35044ed6b1
Varun Kedia <purplevarun@gmail.com>
2022-07-24 14:11:42 +0530

    Extracted MenuContainer into Component

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,43 +1,16 @@
-import { useEffect, useState } from "react";
-import { Animated, Linking, Text, TouchableOpacity, View } from "react-native";
-import useData from "../../context/useData";
+import { Text, View } from "react-native";
 import Contacts from "./components/Contacts";
+import MenuContainer from "./components/MenuContainer";
 const Menu = () => {
-	const { menuOpen, translateX, menuWidth, colors } = useData();
-
-	const [width, setWidth] = useState(0);
-
-	useEffect(() => {
-		if (menuOpen) {
-			setWidth(menuWidth);
-		} else {
-			setTimeout(() => {
-				setWidth(0);
-			}, 500);
-		}
-	}, [menuOpen]);
-
 	return (
-		<Animated.View
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				backgroundColor: colors.primary,
-				height: "100%",
-				width,
-				alignItems: "center",
-				transform: [{ translateX }],
-				justifyContent: "space-between",
-				paddingBottom: 120,
-			}}
-		>
+		<MenuContainer>
 			<View>
 				<Text>hello</Text>
 				<Text>sadhsaui</Text>
 				<Text>safdmnsa</Text>
 			</View>
 			<Contacts />
-		</Animated.View>
+		</MenuContainer>
 	);
 };
 export default Menu;
---
+++ components/menu/components/MenuContainer.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Animated } from "react-native";
+import useData from "../../../context/useData";
+
+const MenuContainer = ({ children }: { children: any }) => {
+	const { menuOpen, translateX, menuWidth, colors } = useData();
+	const [width, setWidth] = useState(0);
+
+	useEffect(() => {
+		if (menuOpen) {
+			setWidth(menuWidth);
+		} else {
+			setTimeout(() => {
+				setWidth(0);
+			}, 500);
+		}
+	}, [menuOpen]);
+
+	return (
+		<Animated.View
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				backgroundColor: colors.primary,
+				height: "100%",
+				width,
+				alignItems: "center",
+				transform: [{ translateX }],
+				justifyContent: "space-between",
+				paddingBottom: 120,
+			}}
+		>
+			{children}
+		</Animated.View>
+	);
+};
+
+export default MenuContainer;
