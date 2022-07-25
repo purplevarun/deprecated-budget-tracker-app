87149f4df0646588f0f6c4cf692ff5faebbbc341
Varun Kedia <purplevarun@gmail.com>
2022-07-25 14:32:01 +0530

    Refactored Login Description

--- screens/login/components/Description.tsx
+++ screens/login/components/Description.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 const Description = () => {
 	return (
 		<Text style={{ marginBottom: 20, fontSize: 20 }}>
-			Enter your email or phone number
+			Enter your username or email
 		</Text>
 	);
 };
