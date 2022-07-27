2666d6b10e63ca64b8170ac1cbfbfe9c6018aceb
Varun Kedia <purplevarun@gmail.com>
2022-07-28 00:48:49 +0530

    Add Date and Add Btn to HomeScreen

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -1,20 +1,37 @@
-import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
 import LongView from "../../components/longview/LongView";
 import colors from "../../context/Data/Colors";
 
 const HomeScreen = () => {
+	const getDate = () => {
+		const fullDate = new Date().toLocaleString();
+		const day = fullDate.slice(0, 10);
+		const year = fullDate.slice(19, 24);
+		return day + year;
+	};
 	return (
 		<LongView
 			style={{
-				justifyContent: "space-between",
 				alignItems: "center",
 				backgroundColor: colors.bg,
-				height: 1000,
+				minHeight: 800,
 			}}
 		>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
+			<View style={{ marginTop: 10, display: "flex", flexDirection: "row" }}>
+				<Text style={{ fontSize: 20 }}>Today's Date : </Text>
+				<Text style={{ fontSize: 20, fontWeight: "bold" }}>{getDate()}</Text>
+			</View>
+
+			<TouchableOpacity
+				style={{
+					position: "absolute",
+					right: 35,
+					top: 650,
+				}}
+			>
+				<Ionicons name="ios-add-circle" size={60} color={colors.primary} />
+			</TouchableOpacity>
 		</LongView>
 	);
 };
